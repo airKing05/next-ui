7ce1ec979addf0c48db0cd65fd20ebee8c6d3ef2
anilraj <anil050797@gmial.com>
2022-01-31 01:21:16 +0530

    footer bugs fixed

--- components/footer/index.js
+++ components/footer/index.js
@@ -6,9 +6,9 @@ import Logo2 from '../../images/logo-2.png';
 
 const Footer = () => {
     return (
-        <div className="container fixed-bottom bg-info" style={{ maxWidth: '100%', padding: '0px !important' }}>
-            <div className="row pt-3 px-5 mb-5 d-flex">
-                <div className="col-6 " >
+        <div className="container bg-info" style={{ maxWidth: '100%', padding: '0px !important' }}>
+            <div className="row d-flex pt-3 px-5 mb-3 d-flex">
+                <div className="col-md-6 mb-4 mb-md-0" >
                     <div className="row d-flex justify-content-center " style={{ maxWidth: '200px', height: '50px' }}>
                         <Image src={Logo2} className="" layout="responsive" alt="brand logo" />
                     </div>
@@ -20,7 +20,7 @@ const Footer = () => {
                     </div>
 
                 </div>
-                <div className="col-3">
+                <div className="col-md-3 mb-4  mb-md-0">
                     <h5 className="">Links</h5>
                     <div className="row">
                         <Link href='/contact'>
@@ -42,11 +42,22 @@ const Footer = () => {
                             </a>
                         </Link>
                     </div>
+                    <div className="row">
+                        <Link href='/contact'>
+                            <a>
+                                <label style={{ fontSize: '18px' }}>
+                                    <i class="fas fa-angle-double-right"></i>
+                                </label>
+                                <span className="" style={{ paddingLeft: '10px', fontSize: '18px' }}>F&#x26;Q</span>
+                            </a>
+                        </Link>
+                    </div>
                 </div>
-                <div className="col-3">
+                <div className="col-md-3 mb-4  mb-md-0">
+                <h5 className="pb-0">Follow on</h5>
                     <div className="row pb-1" >
                         <Link href="https://facebook.com">
-                            <a className="">
+                            <a >
                                 <label style={{ fontSize: '25px', color: 'blue' }}>
                                     <i class="fab fa-facebook align-middle "></i>
                                 </label>
@@ -54,21 +65,21 @@ const Footer = () => {
                             </a>
                         </Link>
                     </div>
-                    <hr style={{ margin: '0px', width: '60%' }} />
-                    <div className="row" >
+                    {/* <hr style={{ margin: '0px', width: '60%' }} /> */}
+                    <div className="row " >
                         <Link href="https://instagram.com">
                             <a className="">
                                 <label style={{ fontSize: '25px', color: 'blue' }}>
                                     <i class="fab fa-instagram align-middle "></i>
                                 </label>
-                                <span className="" style={{ paddingLeft: '10px', fontSize: '18px' }}>Instagram</span>
+                                <span  style={{ paddingLeft: '10px', fontSize: '18px' }}>Instagram</span>
                             </a>
                         </Link>
                     </div>
                 </div>
             </div>
-            <div className="row fixed-bottom " style={{ color: '#fff', background: '#000000', }}>
-                <div className="col text-center py-2">All content © Copyright 2022 by Dekho Krashak All Rights Reserved.</div>
+            <div  style={{ color: '#fff', background: '#000000', maxWidth: '100%'}}>
+                <div className=" text-center py-2 ">All content © Copyright 2022 by Dekho Krashak All Rights Reserved.</div>
             </div>
         </div>
     )
--- components/home/index.js
+++ components/home/index.js
@@ -6,11 +6,11 @@ const HomePage = () => {
     return (
         <>
           <Carousal/>
-            <div className="container mt-5 text-center" style={{maxWidth: '100%', background: '#555555' }}>
+            {/* <div className="container mt-5 text-center" style={{maxWidth: '100%', background: '#555555' }}>
                 
                 <Image src="https://cdn.pixabay.com/photo/2014/09/09/19/07/corn-field-440338_960_720.jpg" width="500" height="300"></Image>
                 <img src="https://cdn.pixabay.com/photo/2020/10/09/20/30/pumpkins-5641482__340.jpg"></img>
-            </div>
+            </div> */}
         </>
 
     )
